efb95ef8d349a728a4dad103395108ca477836f2
Hien Nguyen <hiennguyen92@gmail.com>
2024-03-05 17:05:53 +0700

    feat: update

--- lib/flutter_esim.dart
+++ lib/flutter_esim.dart
@@ -2,14 +2,17 @@ import 'flutter_esim_platform_interface.dart';
 
 class FlutterEsim {
 
+  /// Check support eSIM.
   Future<bool> isSupportESim(List<String>? newer) async {
     return FlutterEsimPlatform.instance.isSupportESim(newer);
   }
 
+  /// Install eSIM.
   Future<String> installEsimProfile(String profile) async {
     return FlutterEsimPlatform.instance.installEsimProfile(profile);
   }
 
+  /// Instructions setup eSIM.
   Future<String> instructions() async {
     return FlutterEsimPlatform.instance.instructions();
   }
